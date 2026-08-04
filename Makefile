fc3b28aa833db8dc5eea98a5605e2cc9b72d7ff8
Stanislaw Pusep <creaktive@gmail.com>
2026-08-04 15:53:10 +0200

    Add music-spectrum with ALSA input

--- Makefile
+++ Makefile
@@ -2,7 +2,10 @@ CFLAGS=-O3 -ffast-math -flto -fomit-frame-pointer -funroll-loops -fno-math-errno
 	   -Wall -Wextra -Wpedantic -Wconversion -Wshadow -Wundef -fno-common
 LDLIBS=-lm
 
-all: benchmark
+all: benchmark music-spectrum
+
+music-spectrum: music-spectrum.o 12tet.o vqsdft.o
+	$(CC) $(CFLAGS) -o $@ $^ $(LDLIBS) -lasound
 
 benchmark: benchmark.o 12tet.o vqsdft.o
 
@@ -10,6 +13,6 @@ pretty:
 	clang-format -i --sort-includes *.[ch]
 
 clean:
-	rm -f *.o benchmark
+	rm -f *.o benchmark music-spectrum
 
 .PHONY: all clean pretty
---
+++ music-spectrum.c
@@ -0,0 +1,101 @@
+#include <alsa/asoundlib.h>
+#include <stdio.h>
+#include <time.h>
+
+#include "12tet.h"
+#include "vqsdft.h"
+
+#define SAMPLE_RATE 24000
+#define BLOCK_SIZE 240
+
+snd_pcm_t *alsa_init(const char *device) {
+  int err;
+  snd_pcm_hw_params_t *params;
+  snd_pcm_t *handle;
+
+  if ((err = snd_pcm_open(&handle, device, SND_PCM_STREAM_CAPTURE, 0)) < 0) {
+    printf("Cannot open audio device: %s\n", snd_strerror(err));
+    exit(1);
+  }
+
+  snd_pcm_hw_params_alloca(&params);
+  snd_pcm_hw_params_any(handle, params);
+  snd_pcm_hw_params_set_access(handle, params, SND_PCM_ACCESS_RW_INTERLEAVED);
+  snd_pcm_hw_params_set_format(handle, params, SND_PCM_FORMAT_S16_LE);
+  snd_pcm_hw_params_set_channels(handle, params, 1);
+  snd_pcm_hw_params_set_rate(handle, params, SAMPLE_RATE, 0);
+
+  if ((err = snd_pcm_hw_params(handle, params)) < 0) {
+    printf("Cannot set hardware parameters: %s\n", snd_strerror(err));
+    exit(2);
+  }
+
+  snd_pcm_prepare(handle);
+  return handle;
+}
+
+ssize_t alsa_read(snd_pcm_t *handle, int16_t *buffer, size_t frames) {
+  int err = (int)snd_pcm_readi(handle, buffer, frames);
+  if (err < 0) {
+    err = snd_pcm_recover(handle, err, 0);
+    if (err < 0) {
+      printf("Read error: %s\n", snd_strerror(err));
+      exit(3);
+    }
+    return 0;
+  }
+  return err;
+}
+
+static const char HEX_LUT[] = "0123456789abcdef";
+
+void dump_spectrum(const VQsDFT *v, int threshold) {
+  for (int i = 0; i < v->num_coeffs; i++) {
+    uint8_t val = (uint8_t)(((v->spectrum_data[i] * 255) + 32768) >> Q_SHIFT);
+    if (val > threshold)
+      val = 0;
+    putchar(HEX_LUT[val >> 4]);
+    putchar(HEX_LUT[val & 0x0F]);
+  }
+  putchar('\n');
+}
+
+int main(int argc, char *argv[]) {
+  const char *device = (argc > 1) ? argv[1] : "plug:dsnoop";
+  snd_pcm_t *handle = alsa_init(device);
+
+  FreqBand bands[MAX_BANDS];
+  generate_12tet_bands(bands, 36, MAX_BANDS);
+
+  double window[2] = {1.0, 0.5};
+
+  VQsDFT dft_instance;
+  vqsdft_init(&dft_instance, bands, MAX_BANDS, window, 2,
+              40.0, // temporal smoothing window in ms
+              SAMPLE_RATE);
+
+  int16_t alsa_samples[BLOCK_SIZE];
+  int32_t q16_samples[BLOCK_SIZE];
+
+  while (true) {
+    ssize_t len;
+    if ((len = alsa_read(handle, alsa_samples, BLOCK_SIZE)) != BLOCK_SIZE) {
+      if (len == 0)
+        break;
+      if (len < 0) {
+        printf("Read returned error\n");
+        exit(4);
+      }
+      continue;
+    }
+
+    for (int i = 0; i < BLOCK_SIZE; i++)
+      q16_samples[i] = ((int32_t)alsa_samples[i]) << 1;
+
+    vqsdft_analyze_block(&dft_instance, q16_samples, BLOCK_SIZE, false);
+
+    dump_spectrum(&dft_instance, 12);
+  }
+
+  return 0;
+}
